1690e5dc483d92dda7ea881ba943c4342a2b987e
Артем Бичеров <jaineut@gmail.com>
2026-05-16 22:20:55 +0300

    Format code in the views module

--- src/views.py
+++ src/views.py
@@ -11,8 +11,11 @@ def aggregate_json(chosen_date: str, range_date: str = "M") -> str:
     expenses_dict = get_frame_expenses(sector_dates_df)
     income_dict = get_frame_income(sector_dates_df)
     curr_stock_prices = get_currency_stock_prices(chosen_date)
-    events_response = {"expenses": expenses_dict, "income": income_dict,
-                       "currency_rates": curr_stock_prices["currency_rates"],
-                       "stock_prices": curr_stock_prices["stock_prices"]}
+    events_response = {
+        "expenses": expenses_dict,
+        "income": income_dict,
+        "currency_rates": curr_stock_prices["currency_rates"],
+        "stock_prices": curr_stock_prices["stock_prices"],
+    }
     json_data = json.dumps(obj=events_response, ensure_ascii=False, indent=4)
     return json_data
