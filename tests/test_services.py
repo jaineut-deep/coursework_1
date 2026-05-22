f70bf5eec7e4c249d293017a3f885731df185714
Артем Бичеров <jaineut@gmail.com>
2026-05-22 12:26:12 +0300

    Add typing to test_services

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,6 +1,6 @@
 from src.services import aggregate_private_transfer
 
 
-def test_agg_private_trans(get_private_trans_json) -> None:
+def test_agg_private_trans(get_private_trans_json: str) -> None:
     result = aggregate_private_transfer()
     assert result == get_private_trans_json
